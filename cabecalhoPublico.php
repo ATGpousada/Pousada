02ecd608c40b078fa5831584748ce3694a556f57
mau_gois <gois.mauricio12@gmail.com>
2023-03-08 21:48:56 -0300

    Adicionando comentários no arquivo "cabecalhoPublico.php"

--- cabecalhoPublico.php
+++ cabecalhoPublico.php
@@ -7,16 +7,21 @@
     <title>Cabeçalho</title>
 </head>
 <body>
+    <!-- Cabeçalho -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm p-3 mb-5 bg-body-tertiary rounded">
+        <!-- Conteúdo do cabeçalho -->
         <div class="container-fluid">
+            <!-- Logo -->
             <a class="navbar-brand" href="#" style="width: 165px;">
                 <img src="images/LOGO POUSADA POR EXTENSO.png" alt="Bootstrap" style="width: 100%;">
             </a>
             
+            <!-- Burguer para o responsivo -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             
+            <!-- Menu do cabeçalho -->
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
