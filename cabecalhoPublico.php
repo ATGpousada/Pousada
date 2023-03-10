549ece2f56ee20bc5376ef2a3d0dce82220f4d78
mau_gois <gois.mauricio12@gmail.com>
2023-03-09 22:32:38 -0300

    Continuação do header(não finalizado) - adição de links para icons na pg index principal - criação de variaveis no css

--- cabecalhoPublico.php
+++ cabecalhoPublico.php
@@ -8,11 +8,11 @@
 </head>
 <body>
     <!-- Cabeçalho -->
-    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm p-3 mb-5 bg-body-tertiary rounded">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm mb-5 bg-body-tertiary rounded">
         <!-- Conteúdo do cabeçalho -->
         <div class="container-fluid">
             <!-- Logo -->
-            <a class="navbar-brand" href="#" style="width: 165px;">
+            <a class="navbar-brand ms-4" href="#" style="width: 165px;">
                 <img src="images/LOGO POUSADA POR EXTENSO.png" alt="Bootstrap" style="width: 100%;">
             </a>
             
@@ -22,18 +22,36 @@
             </button>
             
             <!-- Menu do cabeçalho -->
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Lar</a>
+            <div class="collapse navbar-collapse menu" id="navbarTogglerDemo02">
+                <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0 d-flex gap-4">
+                    <li class="nav-item ms-auto me-auto">
+                        <a class="nav-link fs-4 hover-line" aria-current="page" href="#">Home</a>
                     </li>
                     
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                    <li class="nav-item ms-auto me-auto">
+                        <a class="nav-link fs-4 hover-line" href="">Servicos</a>
+                    </li>
+
+                    <li class="nav-item ms-auto me-auto">
+                        <a class="nav-link fs-4 hover-line" href="product/index.php">Produtos</a>
+                    </li>
+
+                    <li class="nav-item ms-auto me-auto">
+                        <a class="nav-link fs-4 hover-line" href="contact/index.php">Contato</a>
                     </li>
                     
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Desabilitado</a>
+                    <li class="nav-item ms-auto me-auto">
+                        <a class="nav-link fs-4 hover-line" href="">Desabilitado</a>
+                    </li>
+
+                    <li class="nav-item mt-auto mb-auto d-flex justify-content-center gap-4">
+                        <a href="">
+                            <i class="text-decoration-none text-secondary bi bi-person-fill fs-2"></i>
+                        </a>
+
+                        <a href="">
+                            <i class="text-decoration-none text-secondary bi bi-bag-check-fill fs-2"></i></i>
+                        </a>
                     </li>
                 </ul>
             </div>
--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <!-- Icons do Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <!-- Icons do FontAwansome -->
+    <script src="https://kit.fontawesome.com/687b2e222f.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
