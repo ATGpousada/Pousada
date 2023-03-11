e7f70325de3cfcdbfa4ff5947b283cd2574dcee3
vitor <vitorfs502@gmail.com>
2023-03-11 20:11:46 -0300

    Criação da Estrutura de Repetição e de Exibição dos dados .

--- destaquePublico.php
+++ destaquePublico.php
@@ -21,6 +21,24 @@
 </head>
 <body>
 
+<!-- Mostrar se a Consulta Retornou Vasia -->
+<?php if ($num_linhas == 0){?>
+        <div class="mx-auto" style="width: 500px;">
+            <h3 class="bg-warning mt-4 mx-auto text-light">
+                Não há Quartos Cadastrados !!!
+            </h3>
+        </div>
+    <?php }?>
+
+<!-- Mostrar se a Consulta Retornou Conteúdo -->
+    <?php if($num_linhas>0){?>
+            <div class="row">
+        <?php do{?>
+
+        <?php }while($row_destaque = $lista->fetch_assoc())?>
+
+    <?php } ?>
+
 </body>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
