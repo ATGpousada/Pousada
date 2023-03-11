3c2a2ca53c0c3954e977cef92c12cf8d17d2e490
vitor <vitorfs502@gmail.com>
2023-03-11 20:08:21 -0300

    Conexão, Verificação e Atribuição de dados.

--- destaquePublico.php
+++ destaquePublico.php
@@ -1,14 +1,27 @@
-<?php ?>
+<?php 
+//Por motivo desconhecido isso interfere no header ERROR 001
+
+//Conexão com o Banco de dados
+    //include 'connection/connect.php';
+//Verificação de Status do Item
+    //$lista = $conn->query("select * from ... where destaque_quarto = 'Sim';");
+//Atribui resultado de consulta
+    //$row_destaque = $lista->fetch_assoc();
+    //$num_linhas = $lista->num_rows;
+?>
 
 <!DOCTYPE html>
 <html lang="pt_BR">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css"> 
     <title>Destaque - Pousada do Sossego</title>
 </head>
 <body>
 
 </body>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 </html>
