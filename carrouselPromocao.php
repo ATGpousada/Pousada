fcb027703ea4d7e90bc6ecb33771f782322fd111
henrique rojo <henriquerojo10@gmail.com>
2023-03-11 21:26:40 -0300

    excluindo <title> dos arquivos publicos

--- carrouselPromocao.php
+++ carrouselPromocao.php
@@ -4,9 +4,7 @@
 <html lang="pt_BR">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carrousel Promoção - Pousada do Sossego</title>
 </head>
 <body>
     <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false" data-bs-interval="false">
