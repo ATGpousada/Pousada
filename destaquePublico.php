90df400db8944b9a91673ba818d9ee76d383a1bd
henrique rojo <henriquerojo10@gmail.com>
2023-03-11 21:24:03 -0300

    excluindo <title> destaquePublico.php

--- destaquePublico.php
+++ destaquePublico.php
@@ -12,7 +12,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Destaque - Pousada do Sossego</title>
 </head>
 <body>
     <!-- Mostrar se a Consulta Retornou Vasia -->
