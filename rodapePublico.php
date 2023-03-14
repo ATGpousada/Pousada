642110f723b8fe24caaae454ad302f8568fffd5e
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-03-14 17:17:02 -0300

    Inicio do Rodapé, não terminado para mudança pro CSS puro

--- rodapePublico.php
+++ rodapePublico.php
@@ -9,36 +9,68 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div class="container-fluid">
+<div class="container-fluid bg-body-tertiary">
 
-        <div class="row text-center" style="background-color: bg-body-tertiary;">
+    <form>
+        <div class="row text-center d-flex p-4" style="margin-bottom: 20px;">
 
-        <form>
-        <div class="col d-flex align-items-center">
-        <label>Email</label>
-        <input type="email" class="form-control" style=" width: 25% !important;" aria-describedby="emailHelp">
-        </div>
-
-        <div class="col d-flex align-items-center">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" style=" width: 25% !important;">
-    </div>
-
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
-         </div>
-
-         <div class="row text-center d-flex align-items-center">
-            <div class="col">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.864326064771!2d-44.1671581844165!3d-23.028753248025442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c438e8edb483d%3A0x8e9481f17412dc06!2sPousada%20do%20Sossego%20Jacare%C3%AD!5e0!3m2!1spt-BR!2sbr!4v1678816281112!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="col-2">
+                <img src="images/LOGO POUSADA POR EXTENSO.png" style="width: 75%; margin-left: 10px;" >
             </div>
 
-            <div class="col">
-                <img src="images/CREDITO.png" alt=""  style="width: 450px;">
-            </div>     
-         </div>
+            <div class="col-4 fw-bold">
+                <i class="bi bi-info-circle" style="font-size: 25px;"></i>
+                Deseja receber ofertas? <br> Cadastre seu email e fique ligado!
+            </div>
+
+            <div class="col-2 d-flex align-items-center">
+                <input type="text" class="form-control" style="width: 100%; margin-left: 10px;" placeholder="Nome">
+            </div>
+
+            <div class="col-2 d-flex align-items-center">
+                <input type="email" class="form-control" style="width: 100%; margin-left: 10px;" placeholder="Email">
+            </div>
+
+            <div class="col-2 d-flex align-items-center">
+                <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+        </div>
+    </form>
+
+    <div class="row text-center d-flex align-items-center">
+
+        <div class="col d-flex" style="width: 100px;">
+
+        <div class="grid" style="width: 100%;">
+
+            <div class="g-col-6" style="margin-right: 450px;"><a href="">HOME</a></div>
+            <div class="g-col-6" style="margin-right: 430px;"><a href="">SERVIÇOS</a></div>
+            <div class="g-col-6" style="margin-right: 430px;"><a href="">CONTATO</a></div>
+
+            <div id="redes" style="margin-top: 30px;">
+                <i class="fa-brands fa-facebook" style="font-size: 70px;"></i>   
+                <i class="fa-brands fa-instagram" style="font-size: 70px;"></i>
+                <i class="fa-brands fa-whatsapp" style="font-size: 70px;"></i>
+            </div>
+        </div>
+
+
+        </div>
+
+        <div class="col">
+            <img src="images/CREDITO.png" style="width: 450px;">
+        </div>     
+
+        <div class="col">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.864326064771!2d-44.1671581844165!3d-23.028753248025442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c438e8edb483d%3A0x8e9481f17412dc06!2sPousada%20do%20Sossego%20Jacare%C3%AD!5e0!3m2!1spt-BR!2sbr!4v1678816281112!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+
     </div>
+
+    <div class="text-center">
+        &copy; Todos os Direitos Reservados á Pousada do Sossego - 2023
+    </div>
+
+</div>
 </body>
 </html>
