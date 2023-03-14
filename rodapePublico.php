366a99e4d37e35f09eb361c4e914d1da6634034e
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-03-14 15:48:10 -0300

    adição do rodapé, adição de imagens e terminar

--- rodapePublico.php
+++ rodapePublico.php
@@ -1,4 +1,6 @@
-<?php ?>
+<?php 
+
+?>
 
 <!DOCTYPE html>
 <html lang="pt_BR">
@@ -7,6 +9,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    
+    <div class="container-fluid">
+
+        <div class="row text-center" style="background-color: bg-body-tertiary;">
+
+        <form>
+        <div class="col d-flex align-items-center">
+        <label>Email</label>
+        <input type="email" class="form-control" style=" width: 25% !important;" aria-describedby="emailHelp">
+        </div>
+
+        <div class="col d-flex align-items-center">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" style=" width: 25% !important;">
+    </div>
+
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+         </div>
+
+         <div class="row text-center d-flex align-items-center">
+            <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.864326064771!2d-44.1671581844165!3d-23.028753248025442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c438e8edb483d%3A0x8e9481f17412dc06!2sPousada%20do%20Sossego%20Jacare%C3%AD!5e0!3m2!1spt-BR!2sbr!4v1678816281112!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+            <div class="col">
+                <img src="images/CREDITO.png" alt=""  style="width: 450px;">
+            </div>     
+         </div>
+    </div>
 </body>
 </html>
