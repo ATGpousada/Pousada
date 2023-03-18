6bd44796a5435f90f754d0d4c8b12752dd3e4ba8
henrique rojo <henriquerojo10@gmail.com>
2023-03-18 19:36:25 -0300

    inserindo o ícone da pousada no title do sobre nós; e incluindo o arquivo de cabeçalho geral e rodapé no sobre nós

--- about/index.php
+++ about/index.php
@@ -13,10 +13,22 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <!-- Icons do FontAwansome -->
     <script src="https://kit.fontawesome.com/687b2e222f.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <!-- Logo no title -->
+    <link rel="icon" type="image/png" href="images/logo/LOGO POUSADA DO SOSSEGO.png"/>
+    <title>Pousada - Sobre Nós</title>
 </head>
 <body>
-    
+    <!-- Início Menu Público -->
+    <?php 
+    include "../cabecalhoGeral/cabecalhoGeral.php";
+    ?>
+    <!-- Fim Menu Público -->
+
+    <!-- Início Rodapé Público -->
+    <?php 
+    include "../rodapeGeral/rodapeGeral.php";
+    ?>
+    <!-- Fim Rodapé Público -->
 </body>
 <!-- Bootstrap javaScript -->
 <script type="text/javascript" src="../js/bootstrap.min.js"></script>
