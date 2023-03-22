4fccab8b5d36a73035797fa029190482947c45aa
mau_gois <gois.mauricio12@gmail.com>
2023-03-21 21:51:57 -0300

    Adicionando cards na página home e deixando fluid apenas os slides=carroussel e o restante container

--- carrouselTurismo.php
+++ carrouselTurismo.php
@@ -10,11 +10,12 @@
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="3000">
-                <img src="images/banners/Ilha_grande.jpg" class="d-block w-100" alt="...">
+                <img src="images/banners/Pontos_turisticos.jpg" class="d-block w-100" alt="...">
+
             </div>
 
             <div class="carousel-item" data-bs-interval="2000">
-                <img src="images/banners/Pontos_turisticos.jpg" class="d-block w-100" alt="...">
+                <img src="images/banners/Ilha_grande.jpg" class="d-block w-100" alt="...">
             </div>
 
             <div class="carousel-item">
