266f226977a2766e0909d7963626fc72500c7785
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-03-22 00:56:40 -0300

    Alteração do rodapéGeral (novo footer nas demais páginas)

--- rodapeGeral/rodapeGeral.php
+++ rodapeGeral/rodapeGeral.php
@@ -1,64 +1,134 @@
 <?php ?>
 
-<footer class="bg-body-tertiary">
+<footer  class="bg-body-tertiary">
 
-    <div class="cor_footer p-4" style="margin-bottom: 30px;">
-        <form id="form_oferta">
-            <div class="flex">
+<div class="cor_footer p-4" style="background: #c2daf2;">
+    <form id="form_oferta">
+        <div class="flex">
 
-                <div class="flex-end w25">
-                    <i class="bi bi-info-circle mx-3" style="font-size: 50px;"></i>
-                    <p class="text_fp mt-2">Deseja receber ofertas? <br> Cadastre seu email Já!</p>
-                </div>
-
-
-                <div class="align-items-center flex-end w30">
-                    <input type="text" id="nome" class="form-control input_oferta"  placeholder="Nome" required>
-                </div>
-
-                <div class="align-items-center flex-end w30">
-                    <input type="email" id="email" class="form-control input_oferta" placeholder="Email" required>
-                </div>
-
-                <div class="d-flex align-items-center justify-content-center w15">
-                    <button type="submit" value="Enviar" id="botao_oferta" class="btn btn-success p-2 botao_form" style="font-weight: bold; font-size: 20px;">Enviar</button>
-                </div>
+            <div class="flex-end w25">
+                <i class="fa-solid fa-square-envelope d-flex align-items-center" style="font-size: 60px;"></i>
+                <span style="margin-left: 10px;">
+                    <p class="text_fp">RECEBA NOSSAS OFERTAS</p>
+                    <p class="text-center" style="font-weight: bold;">Cadastre seu email Já!</p>
+                </span>
             </div>
-        </form>
-    </div>
 
-    <div class="flex-between" style="width: 90%; margin: 0 auto;">
+            <div class="align-items-center flex-end w30">
+                <input type="text" id="nome" class="form-control input_oferta"  placeholder="Nome" required>
+            </div>
+
+            <div class="align-items-center flex-end w30">
+                <input type="email" id="email" class="form-control input_oferta" placeholder="Email" required>
+            </div>
+
+            <div class="d-flex align-items-center justify-content-center w15">
+                <button type="submit" value="Enviar" id="botao_oferta" class="btn btn-primary p-2 botao_form" style="font-weight: bold; font-size: 20px;">Enviar</button>
+            </div>
+        </div>
+    </form>
+</div>
 
 
-        <div id="logo" class="w33">
+<section class="flex-sa pt-3" style="width: 90%; margin: 0 auto;">
+
+        <div class="logo_div">
             <img src="../images/logo/LOGO POUSADA DO SOSSEGO.png" alt="Logo da pousada" class="logo_footer">
+        </div>
 
-            <div id="link" style="margin-top: 50px;">
-                <div class=""><a href="">HOME</a></div>
-                <div class=""><a href="">SERVIÇOS</a></div>
-                <div class=""><a href="">CONTATO</a></div>
+        <article id="Bloco-1" class="flex-between w25 text-center">
 
-                <div id="redes" style="margin-top: 50px;">
-                    <i class="fa-brands fa-facebook" style="font-size: 70px;"></i>   
-                    <i class="fa-brands fa-instagram" style="font-size: 70px;"></i>
-                    <i class="fa-brands fa-whatsapp" style="font-size: 70px;"></i>
+            <div id="mapa" class="p-3">
+                <span class="map_text">
+                    MAPA DO SITE
+                    <hr class="mb-3 map_linha">
+                </span>
+
+                <div>
+                    <div class="mb-1">
+                        <a href="#" class="map_link">HOME</a>
+                    </div>
+
+                    <div class="mb-1">
+                        <a href="#" class="map_link">SERVIÇOS</a>
+                    </div>
+
+                    <div class="mb-1">
+                        <a href="#" class="map_link">SOBRE NÓS</a>
+                    </div>
+
+                    <div>
+                        <a href="#" class="map_link">CONTATO</a>
+                    </div>
+
                 </div>
             </div>
-        </div>
+
+            <div id="telefone" class="p-3">
+
+                <span class="map_text">
+                    TELEFONE
+                    <hr class="mb-3 map_linha">
+                </span>
+
+                <div class="mb-1 text-center">
+                    <span class="map_contato">(21) 4002-8922</span>
+                </div>
+
+                <div class="mb-1 text-center">
+                    <span class="map_contato">(21) 7070-8580</span>
+                </div>
+            </div>
+        </article>
 
 
-        <div id="pagamento" class="w33">
-            <p class="text-center texto_footer">Formas de Pagamento</p>
-            <img src="../images/pagamento.png" alt="formas de pagamento" style="width: 100%; margin-top: 45px;">
-        </div>
+        <article id="Bloco-2" class="flex-between w45 text-center"> 
 
-        <div class="d-flex justify-content-center w33" id="mapa">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.864326064771!2d-44.1671581844165!3d-23.028753248025442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c438e8edb483d%3A0x8e9481f17412dc06!2sPousada%20do%20Sossego%20Jacare%C3%AD!5e0!3m2!1spt-BR!2sbr!4v1678816281112!5m2!1spt-BR!2sbr" width="450" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
+            <div id="endereco" class="p-3">
+                <span class="map_text">
+                    ENDEREÇO
+                    <hr class="mb-3 map_linha">
+                </span>
+
+                <div class="mb-1 text-center">
+                    <span class="map_contato">Rua José Batista Maia n°3 - Conceição de Jacareí, RJ</span>
+                </div>
+            </div>
+
+        
+            <div id="pagamento" class="p-3">
+
+                <span class="map_text">
+                    FORMAS DE PAGAMENTO
+                    <hr class="mb-3 map_linha">
+                </span>
+
+                <span class="img-bancos">
+                    <img src="../images/Pagamento/Banco-do-Brasil.png" alt="Banco do Brasil" class="img_pag">
+                    <img src="../images/Pagamento/bradesco.svg" alt="Banco Bradesco" class="img_pag">
+                    <img src="../images/Pagamento/caixa-logo.svg" alt="Banco Caixa" class="img_pag">
+                    <img src="../images/Pagamento/Itau.png" alt="Banco Itau" class="img_pag"> <br>
+                    <img src="../images/Pagamento/Mercado-Pago.png" alt="Mercado Pago" class="img_pag">
+                    <img src="../images/Pagamento/Paypal.png" alt="Paypal" class="img_pag">
+                    <img src="../images/Pagamento/pix.png" alt="PIX" class="img_pag">
+                </span>
+            </div>
+        </article>
+</section>
+
+    
+<div class="dropdown">
+    <button class="redes">
+        <i class="fa-solid fa-mobile-screen"></i>
+     </button>
+    <div class="drop_link">
+        <a href="#"><i class="fa-brands fa-square-facebook img_redes" style="font-size: 70px; color: blue;"></i></a>
+        <a href="#"><i class="fa-brands fa-square-instagram img_redes" style="font-size: 70px; color: purple;"></i></a>
+        <a href="#"><i class="fa-brands fa-square-whatsapp img_redes" style="font-size: 70px; color: green;"></i></a>
     </div>
+</div>
 
     <div class="text-center" id="copy">
         &copy; Todos os Direitos Reservados á Pousada do Sossego - 2023
     </div>
-
 </footer>
