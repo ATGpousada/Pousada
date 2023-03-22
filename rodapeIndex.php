2d945cee0f2fcb9ac0dac76fcb54dca6a24d4745
Matheus de Castro Souza <matheuscs006@gmail.com>
2023-03-21 22:06:54 -0300

    Finalizando responsivo do rodapé e deixando codigo mais limpo

--- rodapeIndex.php
+++ rodapeIndex.php
@@ -1,8 +1,8 @@
 <?php ?>
 
-<footer>
+<footer  class="bg-body-tertiary">
 
-<div class="cor_footer p-4"  style="background: #c2daf2;">
+<div class="cor_footer p-4" style="background: #c2daf2;">
     <form id="form_oferta">
         <div class="flex">
 
@@ -29,59 +29,92 @@
     </form>
 </div>
 
-<div class="bg-body-tertiary">
-    <div class="flex-sa pt-3" style="width: 90%; margin: 0 auto;">
 
-        <div style="width: 100px;">
+<section class="flex-sa pt-3" style="width: 90%; margin: 0 auto;">
+
+        <div class="logo_div">
             <img src="images/logo/LOGO POUSADA DO SOSSEGO.png" alt="Logo da pousada" class="logo_footer">
         </div>
 
-        <div id="link" class="w25 text-center">
+        <article id="Bloco-1" class="flex-between w25 text-center">
 
-        <span class="wauto">
-            <div class="map_text">MAPA DO SITE</div>
-                <hr class="mb-3 map_linha">
-                <div class="mb-1"><a href="" class="map_link">HOME</a></div>
-                <div class="mb-1"><a href="" class="map_link">SERVIÇOS</a></div>
-                <div class="mb-1"><a href="" class="map_link">SOBRE NÓS</a></div>
-                <div><a href="" class="map_link">CONTATO</a></div>
-            </div>
-
-
-
-            <div id="telefone" class="w25 text-center">
-
-                <div class="map_text">TELEFONE</div>
+            <div id="mapa" class="p-3">
+                <span class="map_text">
+                    MAPA DO SITE
                     <hr class="mb-3 map_linha">
-                    <div class="mb-1 text-center"><span class="map_contato">(21) 4002-8922</span></div>
-                    <div class="mb-1 text-center"><span class="map_contato">(21) 7070-8580</span></div>
-                </div>
-            </span>
+                </span>
 
-            <span class="flex wauto"> 
-
-                <div id="endereco" class="text-center">
-
-                    <div class="map_text">ENDEREÇO</div>
-                        <hr class="mb-3 map_linha">
-                        <div><span class="map_contato">Rua José Batista Maia n°3 - Conceição de Jacareí, RJ</span></div>
+                <div>
+                    <div class="mb-1">
+                        <a href="#" class="map_link">HOME</a>
                     </div>
 
-        
-                <div id="pagamento" class="text-center" style="width: 500px;">
+                    <div class="mb-1">
+                        <a href="#" class="map_link">SERVIÇOS</a>
+                    </div>
 
-                    <div class="map_text">FORMAS DE PAGAMENTO</div>
-                        <hr class="mb-3 map_linha">
-                        <img src="images/Pagamento/Banco-do-Brasil.png" alt="Banco do Brasil" class="img_pag">
-                        <img src="images/Pagamento/bradesco.svg" alt="Banco Bradesco" class="img_pag">
-                        <img src="images/Pagamento/caixa-logo.svg" alt="Banco Caixa" class="img_pag">
-                        <img src="images/Pagamento/Itau.png" alt="Banco Itau" class="img_pag"> <br>
-                        <img src="images/Pagamento/Mercado-Pago.png" alt="Mercado Pago" class="img_pag">
-                        <img src="images/Pagamento/Paypal.png" alt="Paypal" class="img_pag">
-                        <img src="images/Pagamento/pix.png" alt="PIX" class="img_pag">
+                    <div class="mb-1">
+                        <a href="#" class="map_link">SOBRE NÓS</a>
+                    </div>
+
+                    <div>
+                        <a href="#" class="map_link">CONTATO</a>
+                    </div>
+
                 </div>
-            </span>
-    </div>
+            </div>
+
+            <div id="telefone" class="p-3">
+
+                <span class="map_text">
+                    TELEFONE
+                    <hr class="mb-3 map_linha">
+                </span>
+
+                <div class="mb-1 text-center">
+                    <span class="map_contato">(21) 4002-8922</span>
+                </div>
+
+                <div class="mb-1 text-center">
+                    <span class="map_contato">(21) 7070-8580</span>
+                </div>
+            </div>
+        </article>
+
+
+        <article id="Bloco-2" class="flex-between w45 text-center"> 
+
+            <div id="endereco" class="p-3">
+                <span class="map_text">
+                    ENDEREÇO
+                    <hr class="mb-3 map_linha">
+                </span>
+
+                <div class="mb-1 text-center">
+                    <span class="map_contato">Rua José Batista Maia n°3 - Conceição de Jacareí, RJ</span>
+                </div>
+            </div>
+
+        
+            <div id="pagamento" class="p-3">
+
+                <span class="map_text">
+                    FORMAS DE PAGAMENTO
+                    <hr class="mb-3 map_linha">
+                </span>
+
+                <span class="img-bancos">
+                    <img src="images/Pagamento/Banco-do-Brasil.png" alt="Banco do Brasil" class="img_pag">
+                    <img src="images/Pagamento/bradesco.svg" alt="Banco Bradesco" class="img_pag">
+                    <img src="images/Pagamento/caixa-logo.svg" alt="Banco Caixa" class="img_pag">
+                    <img src="images/Pagamento/Itau.png" alt="Banco Itau" class="img_pag"> <br>
+                    <img src="images/Pagamento/Mercado-Pago.png" alt="Mercado Pago" class="img_pag">
+                    <img src="images/Pagamento/Paypal.png" alt="Paypal" class="img_pag">
+                    <img src="images/Pagamento/pix.png" alt="PIX" class="img_pag">
+                </span>
+            </div>
+        </article>
+</section>
 
     
 <div class="dropdown">
@@ -89,14 +122,13 @@
         <i class="fa-solid fa-mobile-screen"></i>
      </button>
     <div class="drop_link">
-        <a href=""><i class="fa-brands fa-square-facebook img_redes" style="font-size: 70px; color: blue;"></i></a>
-        <a href=""><i class="fa-brands fa-square-instagram img_redes" style="font-size: 70px; color: purple;"></i></a>
-        <a href=""><i class="fa-brands fa-square-whatsapp img_redes" style="font-size: 70px; color: green;"></i></a>
+        <a href="#"><i class="fa-brands fa-square-facebook img_redes" style="font-size: 70px; color: blue;"></i></a>
+        <a href="#"><i class="fa-brands fa-square-instagram img_redes" style="font-size: 70px; color: purple;"></i></a>
+        <a href="#"><i class="fa-brands fa-square-whatsapp img_redes" style="font-size: 70px; color: green;"></i></a>
     </div>
 </div>
 
     <div class="text-center" id="copy">
         &copy; Todos os Direitos Reservados á Pousada do Sossego - 2023
     </div>
-</div>
 </footer>
