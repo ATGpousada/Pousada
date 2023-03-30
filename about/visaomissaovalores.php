bee364d6fe91b588d773ecbd65da38f613019a39
henrique rojo <henriquerojo10@gmail.com>
2023-03-30 11:42:06 -0300

    Criação do conteúdo dos arquivos about/descricao e about/visaomissaovalores

---
+++ about/visaomissaovalores.php
@@ -0,0 +1,11 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    
+</body>
+</html>
