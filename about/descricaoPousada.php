5316debd73d71d0ad5ae39cbb50e3ef9aba8f71c
henrique rojo <henriquerojo10@gmail.com>
2023-04-03 02:41:06 -0300

    construindo o banner carrossel dentro da descrição da pousada e colocando duas imagens de tamanho IGUAL para o banner

--- about/descricaoPousada.php
+++ about/descricaoPousada.php
@@ -8,7 +8,31 @@
 <body>
     <div style="display: flex; margin-top: 25px;">
         <div style="display: inline-block; width: 50%;">
-            <img src="../images/banners/Pousada.jpg" alt="" style="width: 100%;">
+            <div id="carrosselDescricao" class="carousel slide">
+                <div class="carousel-inner">
+
+                    <div class="carousel-item active">
+                        <img src="../images/banners/Pontos_turisticos.jpg" alt="" style="width: 100%;">
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="../images/banners/Praia_de_mangaratiba.jpg" alt="" style="width: 100%;">
+                    </div>
+
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carrosselDescricao" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Próximo</span>
+                </button>
+
+                <button class="carousel-control-next" type="button" data-bs-target="#carrosselDescricao" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Anterior</span>
+                </button>
+
+            </div>
+
         </div>
         <div style="display: inline-block; width: 50%">
             <h1>SOBRE A POUSADA</h1>
