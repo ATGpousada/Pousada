dd69f59a5cd0f886d5deff263f3f3c4055361025
henrique rojo <henriquerojo10@gmail.com>
2023-04-03 02:15:49 -0300

    início container de descrição da pousada no sobre nós

--- about/descricaoPousada.php
+++ about/descricaoPousada.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    
+    <div style="display: flex; margin-top: 25px;">
+        <div style="display: inline-block; width: 50%;">
+            <img src="../images/banners/Pousada.jpg" alt="" style="width: 100%;">
+        </div>
+        <div style="display: inline-block; width: 50%">
+            <h1>SOBRE A POUSADA</h1>
+            <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ea itaque consequuntur! Accusamus aliquid assumenda quas magnam praesentium in sequi numquam sunt ut libero. Ab quaerat itaque eaque reiciendis est!
+        </div>
+    </div>
 </body>
 </html>
