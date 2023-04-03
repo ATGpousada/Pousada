b47c7d138f02f454095a70387ddf8da6012b9afd
henrique rojo <henriquerojo10@gmail.com>
2023-04-03 03:39:58 -0300

    inserção no repositório da imagem teste para simular a imagem da equipe e arrumando a composição das divs e da imagem no sobreEquipe

---
+++ about/sobreEquipe.php
@@ -0,0 +1,18 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div style="display: flex; margin-top: 25px; margin-bottom: 25px;">
+        <div style="display: inline-block; width: 50%; text-align:center;">
+            <p>Nós somos a equipe de desenvolvedores que criamos o site da pousada</p>
+        </div>
+        <div style="display: inline-block; width: 50%; text-align:center; ">
+            <img src="../images/teste.jpeg" alt="" style="border-radius: 50%">
+        </div>
+    </div>
+</body>
+</html>
