363a6ec16a41ac5903d2da1c2190ede76d5861f5
henriquerojo <henriquerojo10@gmail.com>
2023-04-04 13:58:20 -0300

    apagando imagem desnecessária e adicionando padding no container de produto

--- product/produtoExposicao.php
+++ product/produtoExposicao.php
@@ -33,7 +33,7 @@
                                     <span class="fa-solid fa-mug-hot" style="color: goldenrod; font-size: 23px"></span>
                                 </span>
                                 <a href="produto_detalhes.php?id_produto=<?php //echo $row_produtos['id_produto'];?>">
-                                    <button class="hidden-xs btn btn-primary" type="submit"> <span class="hidden-xs bi bi-eye" aria-hidden="true"></span> Saiba Mais</button>
+                                    <button class="hidden-xs btn btn-primary" type="submit"> <span class="hidden-xs bi bi-eye" aria-hidden="true"></span>Saiba Mais</button>
                                 </a>
                             </div>
                         </div>
