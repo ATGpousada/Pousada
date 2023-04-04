c6a4a0c0a578b440aa56a0eb60c87e3dc92cc4bd
João <jvacs2019@gmail.com>
2023-04-04 16:06:27 -0300

    card destaque

--- carrouselTurismo.php
+++ carrouselTurismo.php
@@ -7,27 +7,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+    <div id="carouselExampleInterval" class="carousel slide " data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="3000">
-                <img src="images/banners/Pontos_turisticos.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-item active " data-bs-interval="3000">
+                <img src="images/banners/Pontos_turisticos.jpg" class="d-block w-100 rounded" alt="...">
 
             </div>
 
-            <div class="carousel-item" data-bs-interval="2000">
-                <img src="images/banners/Ilha_grande.jpg" class="d-block w-100" alt="...">
-            </div>
-
-            <div class="carousel-item">
-                <img src="images/banners/Lagoa_azul.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-item " data-bs-interval="2000">
+                <img src="images/banners/Ilha_grande.jpg" class="d-block w-100 rounded" alt="...">
             </div>
 
             <div class="carousel-item">
-                <img src="images/banners/Praia_de_mangaratiba.jpg" class="d-block w-100" alt="...">
+                <img src="images/banners/Lagoa_azul.jpg" class="d-block w-100 rounded" alt="...">
             </div>
 
             <div class="carousel-item">
-                <img src="images/banners/Abraão.jpg" class="d-block w-100" alt="...">
+                <img src="images/banners/Praia_de_mangaratiba.jpg" class="d-block w-100 rounded" alt="...">
+            </div>
+
+            <div class="carousel-item">
+                <img src="images/banners/Abraão.jpg" class="d-block w-100 rounded" alt="...">
             </div>
         </div>
     </div>
---
+++ destaqueQuartos.php
@@ -0,0 +1,44 @@
+<?php ?>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/stylecard.css">
+    </head>
+    <body>
+        <div id="card">
+            <div class="card">
+                <img src="images/quartos/quarto1-2.jpg" alt="" />
+                <div>
+                    <h2>Quarto presidencial</h2>
+                    <h3>suite</h3>
+                    <p>
+                        duas camas com vista para a piscina
+                    </p>
+                    <button>Ver mais...</button>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/quartos/quarto1-2.jpg" alt="" />
+                <div>
+                    <h2>Quarto presidencial</h2>
+                    <h3>suite</h3>
+                    <p>
+                        duas camas com vista para a piscina
+                    </p>
+                    <button>Ver mais...</button>
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/quartos/quarto1-2.jpg" alt="" />
+                <div>
+                    <h2>Quarto presidencial</h2>
+                    <h3>suite</h3>
+                    <p>
+                        duas camas com vista para a piscina
+                    </p>
+                    <button>Ver mais...</button>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
