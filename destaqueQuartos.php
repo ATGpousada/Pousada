f211975129ceb2e46ed4d765f3e62b961fcb04d5
Matheuss06 <matheuscs006@gmail.com>
2023-04-05 10:27:34 -0300

    Back-end do Destaque quartos do HOME

--- destaqueQuartos.php
+++ destaqueQuartos.php
@@ -1,44 +1,36 @@
-<?php ?>
+<?php 
+
+include 'connection/connect.php';
+$lista = $connect->query("select * from vwquartos where DESTAQUE = 1 limit 3;");
+$linha = $lista->fetch_assoc();
+$linhas = $lista->num_rows;
+
+?>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/stylecard.css">
-    </head>
-    <body>
-        <div id="card">
-            <div class="card">
-                <img src="images/quartos/quarto1-2.jpg" alt="" />
-                <div>
-                    <h2>Quarto presidencial</h2>
-                    <h3>suite</h3>
-                    <p>
-                        duas camas com vista para a piscina
-                    </p>
-                    <button>Ver mais...</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images/quartos/quarto1-2.jpg" alt="" />
-                <div>
-                    <h2>Quarto presidencial</h2>
-                    <h3>suite</h3>
-                    <p>
-                        duas camas com vista para a piscina
-                    </p>
-                    <button>Ver mais...</button>
-                </div>
-            </div>
-            <div class="card">
-                <img src="images/quartos/quarto1-2.jpg" alt="" />
-                <div>
-                    <h2>Quarto presidencial</h2>
-                    <h3>suite</h3>
-                    <p>
-                        duas camas com vista para a piscina
-                    </p>
-                    <button>Ver mais...</button>
-                </div>
+<head>
+    <link rel="stylesheet" href="css/stylecard.css">
+</head>
+<body>
+
+    <div id="card">
+        <?php do{?>
+        <?php 
+            $texto = $linha['DESCRICAO'];
+            $inicio = 30; // Inicio da string descrição (A partir do caracter 30)
+            $fim = strlen($texto) - 2350; // Limitar final da string descrição
+        ?>
+        <div class="card">
+            <img src="<?php echo $linha['IMAGEM_CAMINHO_1']?>" alt="" />
+            <div>
+                <h2><?php echo $linha['QUARTO']?></h2>
+                <h3><?php echo $linha['TIPO']?></h3>
+                <p>
+                    <?php echo substr($texto, $inicio, $fim)?>...
+                </p>
+                    <button><a href="details/index.php?ID=<?php echo $linha['ID']?>">Ver mais...</a></button>
             </div>
         </div>
-
-    </body>
+        <?php }while($linha = $lista->fetch_assoc());?>
+    </div>
+</body>
 </html>
