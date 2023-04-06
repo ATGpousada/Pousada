0c9eed23bb3e246c4a3bf8b8409af7adcd435ac4
Pedro Henrique <ph3170560@gmail.com>
2023-04-06 12:16:41 -0300

    Botão na pagina de produtos

--- product/produtoExposicao.php
+++ product/produtoExposicao.php
@@ -7,53 +7,16 @@
     <title>Quartos</title>
 </head>
 <body>
-<div class="row w100">
-    <?php //do{?>
-        <div class="col-sm-5 col-md-4">
-            <div class="card">
-                <a href="produto_detalhes.php?id_produto=<?php echo $row_produtos['id_produto']?>">
-                    <img src="../images/quartos/quarto1.jpg" class="img-fluid rounded float-start">
-                </a>
-                
-                <div class="caption text-end">
-                    <h3 class="text-danger text-center">
-                        <strong>Quarto Laranja</strong>
-                    </h3>
-                    
-                    <p class="text-warning text-center">
-                        <strong>Suíte</strong>
-                    </p>
-                    
-                    <p class="text-start">
-                        Este quarto blá blá blá
-                    </p>
-
-                    <div id="icon_botao" class="d-flex justify-content-beetween">
-                        <span class="me-auto">
-                            <span class="bi bi-people-fill" style="color: goldenrod; font-size: 25px"></span>
-                            <span class="bi bi-car-front-fill" style="color: goldenrod; font-size: 25px"></span>
-                            <span class="fas fa-bed" style="color: goldenrod; font-size: 23px"></span>
-                            <span class="fas fa-bath" style="color: goldenrod; font-size: 23px"></span>
-                            <span class="fa-solid fa-mug-hot" style="color: goldenrod; font-size: 23px"></span>
-                        </span>
-
-                        <a href="../details/index.php?ID=<?php //echo $row_produtos['id_produto'];?>">
-                            <button class="hidden-xs btn btn-primary" type="submit"> <span class="hidden-xs bi bi-eye" aria-hidden="true"></span>Saiba Mais</button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php //}while ($row_produtos = $lista->fetch_assoc())?>
+    
 
     <div class="PARTIÇÃO">
-        <div>
-            <img src="../images/quartos/quarto1.jpg" alt="" style="width: 600px;">
+        <div >
+            <img src="../images/quartos/quarto1.jpg" alt="" style="width: 600px;border-radius:30px;">
         </div>
 
         <div>
             <div class="centraliza_q1">
-                <h1>Suite com varanda</h1>
+                <h1 class="font">Suite com varanda</h1>
             </div>
             
             <div class="distribuição">
@@ -85,7 +48,21 @@
                 <span class="fas fa-paw" style="font-size: 50px;"></span>
                 <h6>Pets</h6> 
             </div>
-        </div> 
+
+            <div class="container">
+    <div class="center">
+      <button class="btn_p">
+        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+         
+          <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+        </svg>
+        <span>Saiba Mais ...</span>
+      </button>
     </div>
+  </div>
+  </div> 
+</div>
+
+
 </body>
 </html>
