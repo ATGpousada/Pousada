211b6c933f6a6254315d501c2865a753d84f56fe
Matheuss06 <matheuscs006@gmail.com>
2023-04-07 00:28:37 -0300

    back-end pronto do destaqueQuartos (Imagens, tipo, nome e ID)

--- destaqueQuartos.php
+++ destaqueQuartos.php
@@ -1,7 +1,17 @@
 <?php 
 
 include 'connection/connect.php';
-$lista = $connect->query("select * from vwquartos where DESTAQUE = 1 limit 3;");
+
+// PESQUISA NO BANCO DE TRÊS TABELAS
+$lista = $connect->query("SELECT quartos.ID, quartos.QUARTO, quartos.DESCRICAO, quartos.destaque, imagens.IMAGEM_CAMINHO_1, quartos.tipos_ID, tipos.tipo
+FROM quartos
+INNER JOIN imagens
+ON quartos.id = imagens.quartos_ID 
+INNER JOIN tipos
+ON quartos.tipos_ID = tipos.id
+WHERE DESTAQUE = 0 
+GROUP BY quartos.ID 
+LIMIT 3");
 $linha = $lista->fetch_assoc();
 $linhas = $lista->num_rows;
 
@@ -14,18 +24,13 @@ $linhas = $lista->num_rows;
 
     <div id="card">
         <?php do{?>
-        <?php 
-            $texto = $linha['DESCRICAO'];
-            $inicio = 30; // Inicio da string descrição (A partir do caracter 30)
-            $fim = strlen($texto) - 2350; // Limitar final da string descrição
-        ?>
         <div class="card">
             <img src="<?php echo $linha['IMAGEM_CAMINHO_1']?>" alt="" />
             <div>
                 <h2><?php echo $linha['QUARTO']?></h2>
-                <h3><?php echo $linha['TIPO']?></h3>
+                <h3><?php echo $linha['tipo']?></h3>
                 <p>
-                    <?php echo substr($texto, $inicio, $fim)?>...
+                    <?php echo substr($linha['DESCRICAO'], 30, 70)?>...
                 </p>
                     <button><a href="details/index.php?ID=<?php echo $linha['ID']?>">Ver mais...</a></button>
             </div>
