4cc43c249107b4ad97528596cce4e69077e1c923
henrique rojo <henriquerojo10@gmail.com>
2023-04-08 01:12:05 -0300

    voltando o produtoExposicao pro jeito mais novo

--- product/produtoExposicao.php
+++ product/produtoExposicao.php
@@ -29,38 +29,61 @@ $linhas = $lista->num_rows;
     <!-- FIM MOSTRAR SE A CONSULTA RETORNAR VAZIO -->
     <!-- ÍNICIO SE A CONSULTA NÃO RETORNAR VAZIO -->
     <div class="row w100">
-            <?php do{?>
-                <div class="col-sm-5 col-md-4">
-                    <div class="card">
-                        <a href="produto_detalhes.php?id_produto=<?php echo $row_produtos['id_produto']?>">
-                            <img src="<?php echo $linha['IMAGEM_CAMINHO_2']?>" class="img-fluid rounded float-start">
-                        </a>
-                        <div class="caption text-end">
-                            <h3 class="text-danger text-center">
-                                <strong><?php echo $linha ['QUARTO']?></strong>
-                            </h3>
-                            <p class="text-warning text-center">
-                                <strong>Suíte</strong>
-                            </p>
-                            <p class="text-start">
-                                Este quarto blá blá blá
-                            </p>
-                            <div id="icon_botao" class="d-flex justify-content-beetween">
-                                <span class="me-auto">
-                                    <span class="bi bi-people-fill" style="color: goldenrod; font-size: 25px"></span>
-                                    <span class="bi bi-car-front-fill" style="color: goldenrod; font-size: 25px"></span>
-                                    <span class="fas fa-bed" style="color: goldenrod; font-size: 23px"></span>
-                                    <span class="fas fa-bath" style="color: goldenrod; font-size: 23px"></span>
-                                    <span class="fa-solid fa-mug-hot" style="color: goldenrod; font-size: 23px"></span>
-                                </span>
-                                <a href="../details/index.php?ID=<?php echo $linha['ID'];?>">
-                                    <button class="hidden-xs btn btn-primary" type="submit"> <span class="hidden-xs bi bi-eye" aria-hidden="true"></span>Saiba Mais</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+    <?php do {?>
+    <div class="PARTIÇÃO">
+        <div>
+            <img src="<?php echo $linha['IMAGEM_CAMINHO_2'];?>" alt="" style="width: 600px;border-radius:30px;">
+        </div>
+
+        <div>
+            <div class="centraliza_q1">
+                <h1 class="font"><?php echo $linha['QUARTO'];?></h1>
+            </div>
+
+            <div class="distribuição">
+                <span class="fas fa-car" style="font-size: 50px;"></span>
+                <h6>Estacionamento</h6>
+            </div>
+
+            <div class="distribuição">
+                <span class="fas fa-bath"style="font-size: 50px;"></span>
+                <h6>Banheiro</h6>
+            </div>
+
+            <div class="distribuição">
+                <span class="fa-solid fa-mug-hot"style="font-size: 50px;"></span>
+                <h6>Café da manhã</h6>
+            </div>
+
+            <div class="distribuição">
+                <span class="fas fa-desktop"style="font-size: 50px;"></span>
+                <h6>Tv Smart</h6> 
+            </div>
+
+            <div class="distribuição">
+                <span class="fas fa-snowflake"style="font-size: 50px;"></span>
+                <h6>Ar condicionado</h6> 
+            </div>
+
+            <div class="distribuição">
+                <span class="fas fa-paw" style="font-size: 50px;"></span>
+                <h6>Pets</h6> 
+            </div>
+
+            <div class="container">
+                <div class="center">
+                    <button class="btn_p">
+                        <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+                            <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                        </svg>
+                        <span>Saiba Mais ...</span>
+                    </button>
                 </div>
-            <?php }while ($linha = $lista->fetch_assoc())?>
+            </div> 
+        </div>
+    </div>
+    <?php } while ($linhas = $lista -> fetch_assoc());?>
+            
     <!-- FIM SE A CONSULTAR NÃO RETORNAR VAZIO -->
 </body>
 </html>
