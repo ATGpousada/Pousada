a2e8342e098b1d52f232edb59fea110858de42b6
henrique rojo <henriquerojo10@gmail.com>
2023-04-09 04:45:38 -0300

    colocando divs lado a lado do missão visão e valores e ajustando o padding 10px no top e bottom

--- about/visaomissaovalores.php
+++ about/visaomissaovalores.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div style="margin-top: 25px;">
-        <div style="background-color: red;">
+    <div style="margin-top: 25px; display:flex;">
+        <div style="background-color: red; display:inline-block; padding-top: 10px; padding-bottom:10px;">
             <div>
                 <span class="fa-solid fa-flag"></span>
             </div>
@@ -18,7 +18,7 @@
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates sapiente perferendis recusandae iure eaque cum unde ullam iusto sed nobis, commodi modi ea aliquid accusantium sint at non incidunt aperiam!
             </div>
         </div>
-        <div style="background-color: blue;">
+        <div style="background-color: blue; display:inline-block; padding-top: 10px; padding-bottom:10px;">
             <div>
                 <span class="fa-sharp fa-solid fa-eye"></span>
             </div>
@@ -29,7 +29,7 @@
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Error in molestias nemo id delectus iusto cumque consequatur atque nisi eius dolores perferendis, consequuntur sit qui temporibus voluptatibus sunt excepturi quisquam.
             </div>
         </div>
-        <div style="background-color: yellow;">
+        <div style="background-color: yellow; display:inline-block; padding-top: 10px; padding-bottom:10px;">
             <div>
                 <span class="fa-solid fa-heart"></span>
             </div>
