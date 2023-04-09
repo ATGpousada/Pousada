a213c342906abade7f528612197921d5a537000d
henrique rojo <henriquerojo10@gmail.com>
2023-04-09 04:42:30 -0300

    estruturação dos containers para missão, visão e valores do sobre nós

--- about/visaomissaovalores.php
+++ about/visaomissaovalores.php
@@ -6,6 +6,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    
+    <div style="margin-top: 25px;">
+        <div style="background-color: red;">
+            <div>
+                <span class="fa-solid fa-flag"></span>
+            </div>
+            <div>
+                Missão
+            </div>
+            <div>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates sapiente perferendis recusandae iure eaque cum unde ullam iusto sed nobis, commodi modi ea aliquid accusantium sint at non incidunt aperiam!
+            </div>
+        </div>
+        <div style="background-color: blue;">
+            <div>
+                <span class="fa-sharp fa-solid fa-eye"></span>
+            </div>
+            <div>
+                Visão
+            </div>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Error in molestias nemo id delectus iusto cumque consequatur atque nisi eius dolores perferendis, consequuntur sit qui temporibus voluptatibus sunt excepturi quisquam.
+            </div>
+        </div>
+        <div style="background-color: yellow;">
+            <div>
+                <span class="fa-solid fa-heart"></span>
+            </div>
+            <div>
+                Valores 
+            </div>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, dignissimos. Omnis molestias accusantium ad eligendi rem recusandae dolorem ipsa, quos rerum nemo quisquam cum a, quis non architecto aperiam labore.
+            </div>
+        </div>
+    </div>
 </body>
 </html>
