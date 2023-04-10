841d27791366582e0df06dd2026db20a14919373
henrique rojo <henriquerojo10@gmail.com>
2023-04-10 03:44:11 -0300

    centrailzando texto através do display:flex com um container no sobreEquipe.php

--- about/sobreEquipe.php
+++ about/sobreEquipe.php
@@ -7,16 +7,18 @@
 </head>
 <body>
     <div style="display: flex; margin-top: 50px; margin-bottom: 25px;">
-        <div style="display: inline-block; width: 50%; text-align:center;">
-        <ul style="list-style: none; text-align:left;">
-            <li><span class="fas fa-paw"></span> <span>ACEITAMOS SEU PET !</span></li>
-            <li><span class="fa-solid fa-mug-hot"></span> <span>CAFÉ DA MANHÃ TODOS OS DIAS DAS 07h00 ÀS 10h00 !</span></li>
-            <li><span class="fa-solid fa-bath"></span> <span>BANHEIRO EM TODOS OS QUARTOS !</span></li>
-            <li><span class="fa-solid fa-car"></span> <span>ESTACIONAMENTO GRATUITO !</span></li>
-            <li><span class="fa-solid fa-bed"></span> <span>SERVIÇO DE QUARTO DIARIO !</span></li>
-        </ul>
+        <div style="display: inline-block; width: 60%; text-align:center;">
+            <div style="display: flex; width: 100%; flex-flow: row nowrap; justify-content:center; align-content:center; height:100%; align-items:center;">
+                <ul style="list-style: none; text-align:left;">
+                    <li><span class="fas fa-paw"></span> <span>ACEITAMOS SEU PET !</span></li>
+                    <li><span class="fa-solid fa-mug-hot"></span> <span>CAFÉ DA MANHÃ TODOS OS DIAS DAS 07h00 ÀS 10h00 !</span></li>
+                    <li><span class="fa-solid fa-bath"></span> <span>BANHEIRO EM TODOS OS QUARTOS !</span></li>
+                    <li><span class="fa-solid fa-car"></span> <span>ESTACIONAMENTO GRATUITO !</span></li>
+                    <li><span class="fa-solid fa-bed"></span> <span>SERVIÇO DE QUARTO DIARIO !</span></li>
+                </ul>
+            </div>
         </div>
-        <div style="display: inline-block; width: 50%; text-align:center; ">
+        <div style="display: inline-block; width: 40%; text-align:center; ">
             <img src="../images/logo/LOGO POUSADA DO SOSSEGO.png" alt="" style="border-radius: 50%; width:60%">
         </div>
     </div>
