8162027a57b643b99be1562ac6864638e45801ba
henrique rojo <henriquerojo10@gmail.com>
2023-04-10 03:53:50 -0300

    tirando border-radius desnecessário no <style> da <img> em sobreEquipe.php

--- about/sobreEquipe.php
+++ about/sobreEquipe.php
@@ -10,16 +10,16 @@
         <div style="display: inline-block; width: 60%; text-align:center;">
             <div style="display: flex; width: 100%; flex-flow: row nowrap; justify-content:center; align-content:center; height:100%; align-items:center;">
                 <ul style="list-style: none; text-align:left;">
-                    <li><span class="fas fa-paw" style="font-size: 22px;"></span>  <span style="font-weight: bolder;">ACEITAMOS SEU PET</span></li>
-                    <li><span class="fa-solid fa-mug-hot" style="font-size: 22px;"></span>  <span style="font-weight: bolder;">CAFÉ DA MANHÃ TODOS OS DIAS DAS 07h00 ÀS 10h00</span></li>
-                    <li><span class="fa-solid fa-bath" style="font-size: 22px;"></span>  <span style="font-weight: bolder;">BANHEIRO EM TODOS OS QUARTOS</span></li>
-                    <li><span class="fa-solid fa-car" style="font-size: 22px;"></span>  <span style="font-weight: bolder;">ESTACIONAMENTO GRATUITO</span></li>
-                    <li><span class="fa-solid fa-bed" style="font-size: 22px;"></span>  <span style="font-weight: bolder;">SERVIÇO DE QUARTO DIÁRIO</span></li>
+                    <li><span class="fas fa-paw" style="font-size: 22px;"></span> <span style="font-weight: bolder;">ACEITAMOS SEU PET</span></li>
+                    <li><span class="fa-solid fa-mug-hot" style="font-size: 22px;"></span> <span style="font-weight: bolder;">CAFÉ DA MANHÃ TODOS OS DIAS DAS 07h00 ÀS 10h00</span></li>
+                    <li><span class="fa-solid fa-bath" style="font-size: 22px;"></span> <span style="font-weight: bolder;">BANHEIRO EM TODOS OS QUARTOS</span></li>
+                    <li><span class="fa-solid fa-car" style="font-size: 22px;"></span> <span style="font-weight: bolder;">ESTACIONAMENTO GRATUITO</span></li>
+                    <li><span class="fa-solid fa-bed" style="font-size: 22px;"></span> <span style="font-weight: bolder;">SERVIÇO DE QUARTO DIÁRIO</span></li>
                 </ul>
             </div>
         </div>
         <div style="display: inline-block; width: 40%; text-align:center; ">
-            <img src="../images/logo/LOGO POUSADA DO SOSSEGO.png" alt="" style="border-radius: 50%; width:60%">
+            <img src="../images/logo/LOGO POUSADA DO SOSSEGO.png" alt="" style="width:60%">
         </div>
     </div>
 </body>
