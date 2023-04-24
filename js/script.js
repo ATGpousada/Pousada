2c905d819565b67dca6f46d2d6bfc82e9c0d0983
vitor502 <vitorfs502@gmail.com>
2023-04-24 16:40:34 -0300

    mascara do CEP consertada para aceitar 9 caracteres

--- js/script.js
+++ js/script.js
@@ -120,21 +120,19 @@ function mascaraRG(i) {
 
 // Mascara Cep
 function mascaraCEP(i) {
-    var v = i.value;
-    if(!/[\d\-]/.test(v[v.length-1])) { // impede entrar outro caractere que não seja número ou hífen
-      i.value = v.substring(0, v.length-1);
-      return;
-    }
-    i.setAttribute("maxlength", "9");
-    if(v.length == 5) {
-      i.value = i.value + "-";
-    } else if(v.length == 9) {
-      i.value = i.value.substring(0, 8);
-    }
+  var v = i.value;
+  if(!/[\d\-]/.test(v[v.length-1])) { // impede entrar outro caractere que não seja número ou hífen
+    i.value = v.substring(0, v.length-1);
+    return;
   }
-  
-  
-  
+  i.setAttribute("maxlength", "9");
+  if(v.length == 5) {
+    i.value = i.value + "-";
+  } else if(v.length == 9) {
+    i.value = i.value.substring(0, 9);
+  }
+}
+
 
 // ---------------------------- Fim SingUp ----------------------------
 
