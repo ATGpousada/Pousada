596e9a325817ab695add4b6487e8bf9ac8ed0237
PEDRO HENRIQUE TORRES GONCALVES <pedro.htgoncalves1@senacsp.edu.br>
2023-04-26 17:21:52 -0300

    texto abaixo do missaovisao e valores

--- about/descricaoPousada.php
+++ about/descricaoPousada.php
@@ -17,16 +17,15 @@
             </div>  
         </div>
 
-       <h6 style="margin:2px; padding:70px;"><strong>Localização:</strong> A Pousada do Sossego é o lugar ideal para quem busca tranquilidade e conforto durante suas viagens.<br> <br>
-    <strong>Pets:</strong> Uma das principais características da Pousada Sossego é que ela aceita animais de estimação, para que os hóspedes possam trazer seus animais  e aproveitar a estadia com eles. Ideal para quem não quer deixar seus amigos de quatro patas para trás ou prefere viajar com eles.<br><br>
-    <strong> Café da manhã:</strong> Além disso, a pousada oferece café da manhã cortesia diariamente para manter os hóspedes cheios de energia e satisfação. O café da manhã inclui opções saudáveis e deliciosas para todos os gostos, incluindo frutas frescas, pães, bolos e sucos.<br><br> 
-    <strong>Horario:</strong> A Pousada Sossego funciona 24 horas por dia, para que você possa aproveitar sua estadia a qualquer hora do dia ou da noite. Isso é especialmente útil se você chegar tarde da noite ou precisar sair de manhã cedo. <br><br>
-   <strong>Estacionamento: </strong> Para sua comodidade, o estacionamento gratuito está disponível para os hóspedes, para que você não precise se preocupar em encontrar um local seguro para estacionar seu carro durante a estadia.<br><br>
-   <strong>Serviços:</strong> O serviço de quarto também está disponível para aqueles que preferem desfrutar de refeições e bebidas em seus quartos. Este serviço permite que os hóspedes peçam deliciosas comidas e bebidas refrescantes sem sair do quarto. 
-       <br><br> 
-     <strong>  Em resumo, a Pousada do Sossego é uma excelente opção para quem viaja a lazer, a negócios e para quem procura um lugar tranquilo e confortável para fugir do dia a dia. Ambiente aconchegante, café da manhã cortesia, horário de funcionamento 24 horas,
-    Com estacionamento gratuito, serviço de quarto e acomodação que aceita animais de estimação, este é um refúgio de paz e tranquilidade.</h6> </strong>
-    
+       <h6 style="margin:2px; padding:70px 70px 0px 70px;"><strong>Localização:</strong> A Pousada do Sossego é o lugar ideal para quem busca tranquilidade e conforto durante suas viagens.<br> <br>
+<strong>Pets:</strong> Uma das principais características da Pousada Sossego é que ela aceita animais de estimação, para que os hóspedes possam trazer seus animais  e aproveitar a estadia com eles. Ideal para quem não quer deixar seus amigos de quatro patas para trás ou prefere viajar com eles.<br><br>
+<strong> Café da manhã:</strong> Além disso, a pousada oferece café da manhã cortesia diariamente para manter os hóspedes cheios de energia e satisfação. O café da manhã inclui opções saudáveis e deliciosas para todos os gostos, incluindo frutas frescas, pães, bolos e sucos.<br><br> 
+<strong>Horario:</strong> A Pousada Sossego funciona 24 horas por dia, para que você possa aproveitar sua estadia a qualquer hora do dia ou da noite. Isso é especialmente útil se você chegar tarde da noite ou precisar sair de manhã cedo. <br><br>
+<strong>Estacionamento: </strong> Para sua comodidade, o estacionamento gratuito está disponível para os hóspedes, para que você não precise se preocupar em encontrar um local seguro para estacionar seu carro durante a estadia.<br><br>
+<strong>Serviços:</strong> O serviço de quarto também está disponível para aqueles que preferem desfrutar de refeições e bebidas em seus quartos. Este serviço permite que os hóspedes peçam deliciosas comidas e bebidas refrescantes sem sair do quarto. 
+    <br><br> 
+<strong>  Em resumo, a Pousada do Sossego é uma excelente opção para quem viaja a lazer, a negócios e para quem procura um lugar tranquilo e confortável para fugir do dia a dia. Ambiente aconchegante, café da manhã cortesia, horário de funcionamento 24 horas,
+    Com estacionamento gratuito, serviço de quarto e acomodação que aceita animais de estimação, este é um refúgio de paz e tranquilidade.</h6> </strong>   
     </div>
 </body>
 </html>
