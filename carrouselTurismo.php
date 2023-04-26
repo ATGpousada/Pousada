83d442bf71dbd071e3f14c83fe3448eac75d3cd6
PEDRO HENRIQUE TORRES GONCALVES <pedro.htgoncalves1@senacsp.edu.br>
2023-04-26 14:21:53 -0300

    mudança de tempo de troca de imagem do banner dos pontos turisticos no index

--- carrouselTurismo.php
+++ carrouselTurismo.php
@@ -9,24 +9,24 @@
 <body>
     <div id="carouselExampleInterval" class="carousel slide " data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active " data-bs-interval="3000">
+            <div class="carousel-item active " data-bs-interval="5000">
                 <img src="images/banners/Pontos_turisticos.jpg" class="d-block w-100 rounded" alt="...">
 
             </div>
 
-            <div class="carousel-item " data-bs-interval="2000">
+            <div class="carousel-item " data-bs-interval="4000">
                 <img src="images/banners/Ilha_grande.jpg" class="d-block w-100 rounded" alt="...">
             </div>
 
-            <div class="carousel-item">
+            <div class="carousel-item" data-bs-interval="4000">
                 <img src="images/banners/Lagoa_azul.jpg" class="d-block w-100 rounded" alt="...">
             </div>
 
-            <div class="carousel-item">
+            <div class="carousel-item" data-bs-interval="4000">
                 <img src="images/banners/Praia_de_mangaratiba.jpg" class="d-block w-100 rounded" alt="...">
             </div>
 
-            <div class="carousel-item">
+            <div class="carousel-item" data-bs-interval="1000">
                 <img src="images/banners/Abraão.jpg" class="d-block w-100 rounded" alt="...">
             </div>
         </div>
