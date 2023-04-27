8323c36189747b98fa76ed57bc7709598b66e597
PEDRO HENRIQUE TORRES GONCALVES <pedro.htgoncalves1@senacsp.edu.br>
2023-04-27 17:21:09 -0300

    deixando responsivo imagem e texto do sobre nós

--- about/descricaoPousada.php
+++ about/descricaoPousada.php
@@ -6,18 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-        <div style="display:flex; justify-content: space-around; margin-top:20px">
-            <img src="../images/logo/LOGO POUSADA DO SOSSEGO.png" alt="" style="width:30%">
-            <div class="text-center" style="margin-top: 95px;">
+        <div class="dividir">
+            <img src="../images/logo/LOGO POUSADA DO SOSSEGO.png" alt="" class="tamanho_l_sobre ">
+            <div class="text-center margin_titulo">
            <strong> <h3>CONHEÇA  MAIS SOBRE A POUSADA DO SOSSEGO</h3><br></strong>
-            <p style="margin:0 auto;width:90%; font-size:20px;">Nossa pousada é acolhedora e prazerosa, pois o sossego reina neste lugar!
+            <p class="texto_titulo" >Nossa pousada é acolhedora e prazerosa, pois o sossego reina neste lugar!
              Atendemos pessoas que buscam lazer, férias ou qualidade de vida na região de Mangaratiba-RJ.
              Com visuais e belezas naturais estonteantes.</p>
             <br>
             </div>  
         </div>
-
-       <h6 style="margin:2px; padding:70px 70px 0px 70px;"><strong>Localização:</strong> A Pousada do Sossego é o lugar ideal para quem busca tranquilidade e conforto durante suas viagens.<br> <br>
+<div>
+    
+       <h6 class="texto_h6">
+       <strong>Localização:</strong> A Pousada do Sossego é o lugar ideal para quem busca tranquilidade e conforto durante suas viagens.<br> <br>
 <strong>Pets:</strong> Uma das principais características da Pousada Sossego é que ela aceita animais de estimação, para que os hóspedes possam trazer seus animais  e aproveitar a estadia com eles. Ideal para quem não quer deixar seus amigos de quatro patas para trás ou prefere viajar com eles.<br><br>
 <strong> Café da manhã:</strong> Além disso, a pousada oferece café da manhã cortesia diariamente para manter os hóspedes cheios de energia e satisfação. O café da manhã inclui opções saudáveis e deliciosas para todos os gostos, incluindo frutas frescas, pães, bolos e sucos.<br><br> 
 <strong>Horario:</strong> A Pousada Sossego funciona 24 horas por dia, para que você possa aproveitar sua estadia a qualquer hora do dia ou da noite. Isso é especialmente útil se você chegar tarde da noite ou precisar sair de manhã cedo. <br><br>
@@ -25,7 +27,8 @@
 <strong>Serviços:</strong> O serviço de quarto também está disponível para aqueles que preferem desfrutar de refeições e bebidas em seus quartos. Este serviço permite que os hóspedes peçam deliciosas comidas e bebidas refrescantes sem sair do quarto. 
     <br><br> 
 <strong>  Em resumo, a Pousada do Sossego é uma excelente opção para quem viaja a lazer, a negócios e para quem procura um lugar tranquilo e confortável para fugir do dia a dia. Ambiente aconchegante, café da manhã cortesia, horário de funcionamento 24 horas,
-    Com estacionamento gratuito, serviço de quarto e acomodação que aceita animais de estimação, este é um refúgio de paz e tranquilidade.</h6> </strong>   
-    </div>
+    Com estacionamento gratuito, serviço de quarto e acomodação que aceita animais de estimação, este é um refúgio de paz e tranquilidade.</h6> </strong>   <br><br>
+</div>
+
 </body>
 </html>
--- about/visaomissaovalores.php
+++ about/visaomissaovalores.php
@@ -7,41 +7,6 @@
     <link rel="stylesheet" href="../css/stylecard.css">
 </head>
 <body>
-    <!-- <div style="margin-top: 50px;margin-bottom:30px; display:flex; width: 100%; background-color:cornflowerblue; margin-left:0; margin-right:0; max-width:100%;">
-        <div style="background-color: none; display:inline-block; padding-top: 10px; padding-bottom:10px; width: 34%; text-align:center;">
-            <div>
-                <span class="fa-solid fa-flag" style="font-size: 100px;"></span>
-            </div>
-            <div>
-                <h3>Missão</h3>
-            </div>
-            <div style="margin-top: 20px;">
-                <p style="font-weight: bold;">Existimos para dar lazer, qualidade de vida e felicidade para nossos hóspedes!</p>
-            </div>
-        </div>
-        <div style="background-color: none; display:inline-block; padding-top: 10px; padding-bottom:10px; width: 33%; text-align:center;;">
-            <div>
-                <span class="fa-sharp fa-solid fa-eye" style="font-size:100px"></span>
-            </div>
-            <div>
-                <h3>Visão</h3>
-            </div>
-            <div style="margin-top: 20px;">
-                <p style="font-weight: bold;">Queremos melhorar a conexão do hóspede com a nossa pousada através da tecnologia e sustentabilidade!</p>
-            </div>
-        </div>
-        <div style="background-color: none; display:inline-block; padding-top: 10px; padding-bottom:10px; width: 33%; text-align:center;">
-            <div>
-                <span class="fa-solid fa-heart" style="font-size: 100px;"></span>
-            </div>
-            <div>
-                <h3>Valores</h3> 
-            </div>
-            <div style="margin-top: 20px;">
-                <p style="font-weight: bold;">Sustentabilidade, Qualidade de Vida, Felicidade, Sonho e Lazer</p>
-            </div>
-        </div>
-    </div> -->
     <div id="card">
         <div class="card" style="background: #c0ddff ">
             <img src="../images/visao.png" alt="" />
@@ -72,5 +37,7 @@
         </div>
     </div>
     
+
+        
 </body>
 </html>
