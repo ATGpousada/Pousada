77565d428b36db65dc64f2ac097069d417f39ba0
henriquerojo <henriquerojo10@gmail.com>
2023-07-01 22:33:55 -0300

    atualizando valores database e senha

--- connection/connect.php
+++ connection/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost"; //definindo o host que estara o banco, nesse caso o banco esta local
-$database = "id20987598_pousada"; // definindo qual banco de dados vamos usar
+$database = "pousada"; // definindo qual banco de dados vamos usar
 $user = "root"; // definindo qual usuário pode acessar o banco
-$pass = "P@ssw0rd"; // definindo a senha para acessar o banco
+$pass = ""; // definindo a senha para acessar o banco
 $charset = "utf8"; // padrão de acentuação brasileiro 
 $port = "3306"; // porta na qual é usada por padrão pelo sql
 
